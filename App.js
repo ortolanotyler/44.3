8b5046bb567da63cc3d63615100ddf6082a2c35d
Tyler Ortolano <111775236+ortolanotyler@users.noreply.github.com>
2023-11-12 12:19:19 -0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+//main app file for our pokegame rendering . hurray!
+
+import React from "react";
+import Pokegame from "./Pokegame";
+
+const App = () => (
+  <div className="App">
+    <Pokegame />
+  </div>
+);
+
+export default App;
